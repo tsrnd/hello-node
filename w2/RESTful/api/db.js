97776921371faa40854a099ccbb79954ae965f67
Hai Nguyen V <hai.nguyen5@asiantech.vn>
2019-03-15 10:36:33 +0700

    simple RESTful

---
+++ w2/RESTful/api/db.js
@@ -0,0 +1,12 @@
+'use strict';
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "",
+  database: process.env.DB_NAME || "nodejs_api",
+  port: 13306
+});
+
+module.exports = db;
