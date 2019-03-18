90c2dfa2c88b05fbf3814db23be21f69ce36344d
Hai Nguyen V <hai.nguyen5@asiantech.vn>
2019-03-18 11:28:23 +0700

    clean code

--- w2/MongoDB/app.js
+++ w2/MongoDB/app.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 const product = require('./routes/product.route');
 // initialize our express app
 const app = express();
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://127.0.0.1:27017/product';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}));
--- w2/MongoDB/routes/product.route.js
+++ w2/MongoDB/routes/product.route.js
@@ -3,16 +3,7 @@ const router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const product_controller = require('../controllers/product.controller');
-// Set up mongoose connection
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://127.0.0.1:27017/product';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// a simple test url to check that all of our files are communicating correctly.
 router.get('/', product_controller.product_list);
 router.get('/test', product_controller.test);
 router.post('/create', product_controller.product_create);
